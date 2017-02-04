eef7574b0fc646372996fc1899d314e75d89b952
psfrankie <frankie-orourke@pluralsight.com>
2017-02-04 13:35:32 -0800

    add index file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const transforms = require('./transforms')
+const validators = require('./validators')
+const fields = require('./fields')
+const { build } = require('./build')
+
+module.exports = {
+  transforms,
+  validators,
+  fields,
+  build
+}
