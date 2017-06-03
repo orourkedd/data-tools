4fc912474eb0ca0037819cf4043362225e6558a5
psfrankie <frankie-orourke@pluralsight.com>
2017-06-03 16:47:22 -0700

    add date field

--- src/fields.js
+++ src/fields.js
@@ -1,62 +1,65 @@
-const t = require('./transforms')
-const v = require('./validators')
-const uuid = require('uuid')
-const { merge, flatten, filter } = require('ramda')
-const { isUndefined } = require('./utils')
+const t = require('./transforms');
+const v = require('./validators');
+const uuid = require('uuid');
+const { merge, flatten, filter } = require('ramda');
+const { isUndefined } = require('./utils');
 
-function field (name, options) {
-  const s1 = { name }
-  const s2 = merge(s1, options)
+function field(name, options) {
+  const s1 = { name };
+  const s2 = merge(s1, options);
   const s3 = merge(s2, {
     transforms: cleanList(options.transforms),
     validators: cleanList(options.validators)
-  })
-  return s3
+  });
+  return s3;
 }
 
-function toArray (a) {
-  return Array.isArray(a) ? a : [a]
+function toArray(a) {
+  return Array.isArray(a) ? a : [a];
 }
 
-function cleanList (a) {
-  const s1 = flatten([a])
-  const s2 = filter(v => v, s1)
-  return s2
+function cleanList(a) {
+  const s1 = flatten([a]);
+  const s2 = filter(v => v, s1);
+  return s2;
 }
 
-function guid (name, options = {}) {
+function guid(name, options = {}) {
   return field(name, {
     defaultValue: options.defaultValue || uuid.v4,
     transforms: [t.stringTrim],
     validators: [v.notFalsey]
-  })
+  });
 }
 
-function text (name, options = {}) {
-  const validators = toArray(options.validators)
-  if (options.required === true) validators.push(v.notFalsey)
+function text(name, options = {}) {
+  const validators = toArray(options.validators);
+  if (options.required === true) validators.push(v.notFalsey);
 
-  return field(name, merge(options, {
-    defaultValue: '',
-    transforms: [t.stringTrim, options.transforms],
-    validators
-  }))
+  return field(
+    name,
+    merge(options, {
+      defaultValue: '',
+      transforms: [t.stringTrim, options.transforms],
+      validators
+    })
+  );
 }
 
-function url (name, options = {}) {
-  options.validators = [options.validators, v.url]
-  return text(name, options)
+function url(name, options = {}) {
+  options.validators = [options.validators, v.url];
+  return text(name, options);
 }
 
-function phone (name, options = {}) {
-  options.validators = [options.validators, v.phone]
-  options.transforms = [t.stringNumbersOnly, options.transforms]
-  return text(name, options)
+function phone(name, options = {}) {
+  options.validators = [options.validators, v.phone];
+  options.transforms = [t.stringNumbersOnly, options.transforms];
+  return text(name, options);
 }
 
-function address (name, options = {}) {
-  const textOptions = {}
-  if (options.required) textOptions.required = true
+function address(name, options = {}) {
+  const textOptions = {};
+  if (options.required) textOptions.required = true;
   return {
     name,
     fields: [
@@ -66,25 +69,38 @@ function address (name, options = {}) {
       text('state', merge(textOptions, { transforms: [t.stringToUpperCase] })),
       text('zip', textOptions)
     ]
-  }
+  };
 }
 
-function enumeration (name, options = {}) {
-  return field(name, merge(options, {
-    validators: [options.validators, v.enumeration]
-  }))
+function enumeration(name, options = {}) {
+  return field(
+    name,
+    merge(options, {
+      validators: [options.validators, v.enumeration]
+    })
+  );
 }
 
-function boolean (name, options = {}) {
-  return enumeration(name, merge(options, {
-    values: [true, false]
-  }))
+function boolean(name, options = {}) {
+  return enumeration(
+    name,
+    merge(options, {
+      values: [true, false]
+    })
+  );
 }
 
-function number (name, options = {}) {
-  return field(name, merge(options, {
-    validators: [options.validators, v.number]
-  }))
+function number(name, options = {}) {
+  return field(
+    name,
+    merge(options, {
+      validators: [options.validators, v.number]
+    })
+  );
+}
+
+function date(name, options = {}) {
+  return field(name, options);
 }
 
 module.exports = {
@@ -95,5 +111,6 @@ module.exports = {
   address,
   enumeration,
   boolean,
-  number
-}
+  number,
+  date
+};
--- src/validators.js
+++ src/validators.js
@@ -1,62 +1,62 @@
-const isUrl = require('is-url')
+const isUrl = require('is-url');
 
-function notFalsey (value, path) {
-  if (value) return
+function notFalsey(value, path) {
+  if (value) return;
 
   return {
     validator: 'notFalsey',
     path
-  }
+  };
 }
 
-function url (value, path) {
-  if (!value) return
+function url(value, path) {
+  if (!value) return;
   if (!isUrl(value)) {
     return {
       validator: 'url',
       path
-    }
+    };
   }
 }
 
-function phone (value, path) {
-  if (!value) return
+function phone(value, path) {
+  if (!value) return;
   if (value.length !== 10) {
     return {
       validator: 'phone',
       path
-    }
+    };
   }
 }
 
-function enumeration (value, path, fieldDefinition) {
-  const { values } = fieldDefinition
-  if (values.indexOf(value) > -1) return
+function enumeration(value, path, fieldDefinition) {
+  const { values } = fieldDefinition;
+  if (values.indexOf(value) > -1) return;
 
   return {
     validator: 'enumeration',
     path
-  }
+  };
 }
 
-function boolean (value, path) {
-  if (value === undefined) return
+function boolean(value, path) {
+  if (value === undefined) return;
   if (value !== true && value !== false) {
     return {
       validator: 'boolean',
       path
-    }
+    };
   }
 }
 
-function number (value, path) {
-  if (value === undefined) return
-  if (!isNaN(value)) return
+function number(value, path) {
+  if (value === undefined) return;
+  if (!isNaN(value)) return;
 
   return {
     validator: 'number',
     path
-  }
+  };
 }
 
 module.exports = {
@@ -66,4 +66,4 @@ module.exports = {
   enumeration,
   boolean,
   number
-}
+};
